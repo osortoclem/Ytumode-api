821c132b07118efe835d1e59c60d0456ae5b6a1e
deylinqff <maycraftminecraft4@gmail.com>
2025-05-09 17:23:55 -0600

    Actualizar search.js

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
 
   if (req.method === "OPTIONS") return res.status(200).end();
 
